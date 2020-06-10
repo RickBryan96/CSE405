6430c548247bd113641b3a82bf7df35ad8a4424b
RickBryan <55292529+RickBryan96@users.noreply.github.com>
2020-06-09 22:59:59 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const recommendationList = document.querySelector('.recommendations');
+
+//set up recs
+const setupRecommendations = (data) => {
+  let html = '';
+  data.forEach(doc => {
+    const recommendation = doc.data();
+    const li = `
+      <li>
+        <div class="collapsible-header grey lighten-4"> ${recommendation.title} </div>
+       <div class="collapsible-body white"> ${recommendation.content} </div>
+      </li>
+    `;
+   html += li;
+  });
+  recommendationList.innerHTML = html
+
+}
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
     var modals = document.querySelectorAll('.modal');
